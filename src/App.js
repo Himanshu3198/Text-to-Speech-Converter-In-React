30d32f43cb0f8469566c8ba78f7c47954992bddb
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2021-01-19 00:36:02 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+import './App.css';
+import Speech from "./speech";
+function App() {
+  return (
+    <div className="App">
+      <div className="group">
+
+        <h1>Text to Speech Converter</h1>
+      </div>
+      
+       <Speech/>
+    </div>
+  );
+}
+
+export default App;
